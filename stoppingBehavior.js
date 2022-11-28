32244aa7c506d3018811d01d44bad8887e24fc27
Cecilia <ctong10@uw.edu>
2022-11-27 23:26:30 -0800

    completed Stopping Behavior hw

--- stoppingBehavior.js
+++ stoppingBehavior.js
@@ -1,10 +1,23 @@
+/**
+ * Cecilia Tong
+ * Class 5 Exercise & Homework
+ * Nov 22, 2022
+ * Stopping Behavior
+ */
+
 // Do not change
 document.getElementById('cat').addEventListener('click', () => {
   alert('meow!');
 });
 
-// When clicked, "More info" link should alert "Here's some info"
+// 1. When clicked, "More info" link should alert "Here's some info"
 // instead of going to a new webpage
+const aEl = document.getElementById("more-info");
+aEl.addEventListener('click', (e) => {alert(`Here's some more info`)
+e.preventDefault(); })
 
-// When the bark button is clicked, should alert "Bow wow!"
+// 2. When the bark button is clicked, should alert "Bow wow!"
 // Should *not* alert "meow"
+const buttonEl = document.getElementById("dog");
+buttonEl.addEventListener('click', (e) => {alert(`Bow wow!`)
+e.stopPropagation(); })
