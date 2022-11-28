a1031587b741adc3661ce4776b281103eafa18f3
Cecilia <ctong10@uw.edu>
2022-11-27 23:35:25 -0800

    completed jQuery Dom Crud hw

--- jQueryDomCrud.js
+++ jQueryDomCrud.js
@@ -1,15 +1,35 @@
-// Create a new <a> element containing the text "Buy Now!"
-// with an id of "cta" after the last <p>
+/**
+ * Cecilia Tong
+ * Class 5 Exercise & Homework
+ * Nov 22, 2022
+ * jQuery Dom Crud
+ */
 
-// Access (read) the data-color attribute of the <img>,
-// log to the console
+// 1. Wrap your JS code in the jQuery document.ready method
+$(document).ready(function () {
 
-// Update the third <li> item ("Turbocharged"),
-// set the class name to "highlight"
+    // 4. Create a new <a> element containing the text "Buy Now!"
+    // with an id of "cta" after the last <p>
+    var $aEl = $("<a>", { id: "cta" }).text("Buy Now!");
+    $("p").after($aEl); //add after <p>
 
-// Remove (delete) the last paragraph
-// (starts with "Available for purchase now…")
+    // 2. Access (read) the data-color attribute of the <img>,
+    // log to the console
+    var $dataColor = $('img').attr("data-color");
+    console.log(`The data-color of the image is ${$dataColor}`);
 
-// Create a listener on the "Buy Now!" link that responds to a click event.
-// When clicked, the the "Buy Now!" link should be removed
-// and replaced with text that says "Added to cart"
+    // 3. Update the third <li> item ("Turbocharged"),
+    // set the class name to "highlight"
+    $("ul li:eq(2)").addClass("highlight");
+
+    // 5. Remove (delete) the last paragraph
+    // (starts with "Available for purchase now…")
+    $("p:last").remove();
+
+    // 6. Create a listener on the "Buy Now!" link that responds to a click event.
+    // When clicked, the "Buy Now!" link should be removed
+    // and replaced with text that says "Added to cart"
+    $($aEl).click(function () {
+        $($aEl).html("Added to cart");
+    })
+});
